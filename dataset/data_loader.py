92386a2f39bd278b98615e9dc4e80d677680ad4e
VU Manh Tu <glmanhtu@gmail.com>
2022-01-07 11:02:53 +0100

    Update

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -34,7 +34,9 @@ class WriterDataLoader:
         bincount = dict(Counter(labels))
         key_list = list(bincount.keys())
         weight_map = 1 / np.asarray([bincount[x] for x in key_list])
-        weight_map = interp(weight_map, [np.min(weight_map), np.max(weight_map)], [1, weight_max])
+        weight_map = weight_map / np.min(weight_map) * 10
+        weight_map = np.log10(weight_map)
+        # weight_map = interp(weight_map, [np.min(weight_map), np.max(weight_map)], [1, weight_max])
         weights = []
         for key in labels:
             weights.append(weight_map[key_list.index(key)])
