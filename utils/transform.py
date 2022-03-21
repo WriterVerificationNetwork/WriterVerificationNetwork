43598ee3c4e5bd38caddc550f3e985733589a762
VU Manh Tu <glmanhtu@gmail.com>
2022-03-21 07:24:11 +0100

    update

--- utils/transform.py
+++ utils/transform.py
@@ -35,7 +35,7 @@ def get_transforms(args):
             torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3),
         ], p=applying_percent),
         torchvision.transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         #
         # torchvision.transforms.RandomApply([
         #     lambda x: addNoise(x, x.device)
